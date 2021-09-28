c4f5bd4b4af4c754a4405955e0348520745c0fca
Miguel D Islas <l17540230@delicias.tecnm.mx>
2021-09-28 13:47:40 -0600

    second commit

--- lib/Screens/SignUp/sign_up_screen.dart
+++ lib/Screens/SignUp/sign_up_screen.dart
@@ -1,30 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
+///import 'package:firebase_auth/firebase_auth.dart';
 
 import 'components/body.dart';
-import '../Dashboard/dashboard.dart';
+//import '../Dashboard/dashboard.dart';
 
 class SignUp extends StatelessWidget {
   static String routeName = "/signup";
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: const CircularProgressIndicator(),
-              );
-            } else if (snapshot.hasData) {
-              return DashboardScreen();
-            } else if (snapshot.hasError) {
-              return Center(
-                child: const Text('Something went wrong'),
-              );
-            } else {
-              return Body();
-            }
-          },
-        ),
-      );
+  Widget build(BuildContext context) {
+    return Body();
+  }
 }
+
+// class SignUp extends StatelessWidget {
+//   static String routeName = "/signup";
+//   @override
+//   Widget build(BuildContext context) => Scaffold(
+//         body: StreamBuilder(
+//           stream: FirebaseAuth.instance.authStateChanges(),
+//           builder: (context, snapshot) {
+//             if (snapshot.connectionState == ConnectionState.waiting) {
+//               return Center(
+//                 child: const CircularProgressIndicator(),
+//               );
+//             } else if (snapshot.hasData) {
+//               return DashboardScreen();
+//             } else if (snapshot.hasError) {
+//               return Center(
+//                 child: const Text('Something went wrong'),
+//               );
+//             } else {
+//               return Body();
+//             }
+//           },
+//         ),
+//       );
+// }
